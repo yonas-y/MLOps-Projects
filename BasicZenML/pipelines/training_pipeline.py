b0916dd5d113d443ceaf660927d41969db7fe195
Yonas Y <yoni99y@gmail.com>
2025-06-20 16:35:48 +0300

    Added data importing and showing of the shapes in the training pipeline!

--- BasicZenML/pipelines/training_pipeline.py
+++ BasicZenML/pipelines/training_pipeline.py
@@ -1,7 +1,14 @@
-from zenml.pipelines import pipeline
+from zenml import pipeline
+from steps.data_loader import import_digits_data, show_shapes
 
 @pipeline
-def training_pipeline(import_data, train_model, evaluate_model):
-    X, y = import_data()
-    model = train_model(X=X, y=y)
-    evaluate_model(model=model, X=X, y=y)
+def training_pipeline():
+    x_train, y_train, x_test, y_test = import_digits_data()
+    show_shapes(x_train, y_train, x_test, y_test)
+
+
+# @pipeline
+# def training_pipeline(import_data, train_model, evaluate_model):
+#     X, y = import_data()
+#     model = train_model(X=X, y=y)
+#     evaluate_model(model=model, X=X, y=y)

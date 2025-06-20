13b3fdaae06f94170171713dfa94be362aa7a7d9
Yonas Y <yoni99y@gmail.com>
2025-06-20 23:16:32 +0300

    Added a training and testing evaluation steps in the pipeline!

--- BasicZenML/pipelines/training_pipeline.py
+++ BasicZenML/pipelines/training_pipeline.py
@@ -1,16 +1,12 @@
 from zenml import pipeline
 from steps.data_loader import import_digits_data, show_shapes
 from steps.model_trainer import train_model
+from steps.evaluator import evaluate_model
 
 @pipeline
 def training_pipeline():
     x_train, y_train, x_test, y_test = import_digits_data()
     show_shapes(x_train, y_train, x_test, y_test)
     model = train_model(x_train, y_train)
-
-
-# @pipeline
-# def training_pipeline(import_data, train_model, evaluate_model):
-#     X, y = import_data()
-#     model = train_model(X=X, y=y)
-#     evaluate_model(model=model, X=X, y=y)
+    training_accuracy = evaluate_model(model, x_train, y_train)
+    test_accuracy = evaluate_model(model, x_test, y_test)

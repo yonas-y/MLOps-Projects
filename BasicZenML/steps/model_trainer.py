92f42dbf245c0812bed5f28099e06edcdaa81c4d
Yonas Y <yoni99y@gmail.com>
2025-06-20 16:51:55 +0300

    Updated the model training!

--- BasicZenML/steps/model_trainer.py
+++ BasicZenML/steps/model_trainer.py
@@ -1,8 +1,8 @@
 from zenml.steps import step
 from sklearn.ensemble import RandomForestClassifier
-
+import numpy as np
 @step
-def train_model(X: list, y: list) -> RandomForestClassifier:
+def train_model(X: np.ndarray, y: np.ndarray) -> RandomForestClassifier:
     model = RandomForestClassifier()
     model.fit(X, y)
     return model

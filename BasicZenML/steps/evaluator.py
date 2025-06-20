dc5a696126e0ecc64a32e9a137edc1a8b5f597bf
Yonas Y <yoni99y@gmail.com>
2025-06-20 23:09:22 +0300

    Updated the evaluator script!

--- BasicZenML/steps/evaluator.py
+++ BasicZenML/steps/evaluator.py
@@ -1,8 +1,9 @@
 from zenml.steps import step
 from sklearn.metrics import accuracy_score
+import numpy as np
 
 @step
-def evaluate_model(model, X: list, y: list) -> float:
+def evaluate_model(model, X: np.ndarray, y: np.ndarray) -> float:
     preds = model.predict(X)
     acc = accuracy_score(y, preds)
     print(f"Accuracy: {acc}")

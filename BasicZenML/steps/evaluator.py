fc5f9afedc107ce1f7c379d1f65bad18620f45a1
Yonas Y <yoni99y@gmail.com>
2025-06-20 23:15:06 +0300

    Added a logger to the evaluator script!

--- BasicZenML/steps/evaluator.py
+++ BasicZenML/steps/evaluator.py
@@ -1,10 +1,12 @@
 from zenml.steps import step
 from sklearn.metrics import accuracy_score
 import numpy as np
+import logging
 
+logger = logging.getLogger(__name__)
 @step
 def evaluate_model(model, X: np.ndarray, y: np.ndarray) -> float:
     preds = model.predict(X)
     acc = accuracy_score(y, preds)
-    print(f"Accuracy: {acc}")
+    logger.info(f"Accuracy: {acc}")
     return acc

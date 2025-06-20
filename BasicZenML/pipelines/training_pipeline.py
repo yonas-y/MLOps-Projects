adc234fc2d5bcc87d13ee7175c9d95e4320a6b88
Yonas Y <yoni99y@gmail.com>
2025-06-20 16:52:42 +0300

    Added the model training in the training pipeline!

--- BasicZenML/pipelines/training_pipeline.py
+++ BasicZenML/pipelines/training_pipeline.py
@@ -1,10 +1,12 @@
 from zenml import pipeline
 from steps.data_loader import import_digits_data, show_shapes
+from steps.model_trainer import train_model
 
 @pipeline
 def training_pipeline():
     x_train, y_train, x_test, y_test = import_digits_data()
     show_shapes(x_train, y_train, x_test, y_test)
+    model = train_model(x_train, y_train)
 
 
 # @pipeline

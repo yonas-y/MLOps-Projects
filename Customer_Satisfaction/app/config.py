5de1fadae8b1d42f8aac00c5db3448f0817fd772
Yonas Y <yoni99y@gmail.com>
2025-06-23 09:22:58 +0300

    Updated the config file!

--- Customer_Satisfaction/app/config.py
+++ Customer_Satisfaction/app/config.py
@@ -4,6 +4,8 @@ Contains Configuration (DB URI, names, paths).
 
 # app/config.py
 MONGO_URI = "mongodb://192.168.80.1:27017"  # The address to the local MongoDB!
+HOST_IP = "192.168.80.1"
+HOST_PORT = 27017
 DB_NAME = "Customers-Satisfaction"
 COLLECTION_NAME = "Customers"
 MODEL_DIR = "output"
